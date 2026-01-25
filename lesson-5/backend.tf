265446b9cebaf9aac36c4b4c1d71d02a4975bd3c
bedu1441 <b.stoietskyi@gmail.com>
2026-01-24 19:50:03 -0500

    Fix HW5: configure Terraform S3 backend

---
+++ lesson-5/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "bedu1441-terraform-state"
+    key            = "lesson-5/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
