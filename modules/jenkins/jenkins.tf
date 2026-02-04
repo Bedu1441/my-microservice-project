bde52792f171ceb1f63c3bbe065b5e998d11cb22
Bedu1441 <b.stoietskyi@gmail.com>
2026-02-03 20:45:39 -0500

    fix: set jenkins persistence storageClass gp2 and increase timeout

---
+++ modules/jenkins/jenkins.tf
@@ -0,0 +1,29 @@
+resource "kubernetes_namespace_v1" "jenkins" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "kubernetes_service_account_v1" "jenkins_agent" {
+  metadata {
+    name      = "jenkins-agent"
+    namespace = kubernetes_namespace_v1.jenkins.metadata[0].name
+  }
+}
+
+resource "helm_release" "jenkins" {
+  name       = var.release_name
+  namespace  = kubernetes_namespace_v1.jenkins.metadata[0].name
+  repository = "https://charts.jenkins.io"
+  chart      = "jenkins"
+  version    = var.chart_version
+
+  values = [file("${path.module}/values.yaml")]
+
+  timeout = 1800
+
+  depends_on = [
+    kubernetes_namespace_v1.jenkins,
+    kubernetes_service_account_v1.jenkins_agent
+  ]
+}
